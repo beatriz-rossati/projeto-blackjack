d38bd9c66dfd6b01fbb12c933d610930efd338e0
beatriz-schanner <biaschanner@gmail.com>
2023-04-07 13:54:56 -0300

    Desenvolvimento do Projeto BlackJack versão Desafio

--- desafio.js
+++ desafio.js
@@ -1,13 +1,67 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+alert("Boas-vindas ao jogo de BlackJack!")
 
+while (confirm("Deseja iniciar uma nova partida?")) {
+    iniciarPartida()
+}
+
+alert("O jogo acabou")
+
+function iniciarPartida() {
+    let cartasJogador = [comprarCarta(), comprarCarta()]
+    // testando 2 As hardcodado
+    // let cartasJogador = [{texto: "AC", valor: 11},{texto: "AP", valor: 11}]
+    let cartasComputador = [comprarCarta(), comprarCarta()]
+
+    if (cartasJogador[0].valor == 11 && cartasJogador[1].valor == 11) {
+        cartasJogador = [comprarCarta(), comprarCarta()]
+        console.log("dois As para jogador, resorteando")
+    }
+    if (cartasComputador[0].valor == 11 && cartasComputador[1].valor == 11) {
+        cartasComputador = [comprarCarta(), comprarCarta()]
+        console.log("dois As para computador, resorteando")
+    }
+
+    let pontJogador = cartasJogador[0].valor + cartasJogador[1].valor
+    let pontComputador = cartasComputador[0].valor + cartasComputador[1].valor
+
+    let mensagemJogador = `Cartas Jogador: ${cartasJogador.map(carta => carta.texto)} e a soma delas é ${pontJogador}.`
+    let mensagemComputador = `Carta revelada do Computador: ${cartasComputador[0].texto} e o valor delas é ${cartasComputador[0].valor}.`
+
+    while (pontJogador < 21 && perguntarJogador() == true) {
+        const novaCarta = comprarCarta()
+        cartasJogador.push(novaCarta)
+        pontJogador += novaCarta.valor
+        mensagemJogador = `Cartas Jogador: ${cartasJogador.map(carta => carta.texto)} e a soma delas é ${pontJogador}`
+    }
+
+    while (pontComputador < 21 && pontJogador < 21 && pontComputador < pontJogador) {
+        const novaCarta = comprarCarta()
+        cartasComputador.push(novaCarta)
+        pontComputador += novaCarta.valor
+
+    }
+
+    mensagemComputador = `Cartas Computador: ${cartasComputador.map(carta => carta.texto)} e a soma delas é ${pontComputador}.`
+
+    alert(`
+    ${mensagemJogador} 
+    ${mensagemComputador} 
+    ${resultadoJogo(pontJogador, pontComputador)}`)
+
+    function perguntarJogador() {
+        return confirm(`
+            ${mensagemJogador}
+            ${mensagemComputador}
+            Deseja comprar mais uma carta?`)
+    }
+
+    function resultadoJogo(resultadoJogador, resultadoComputador) {
+        if (((resultadoJogador > resultadoComputador) || (resultadoComputador > 21)) && (resultadoJogador <= 21)) {
+            return ("Você ganhou!")
+        } else
+            if (((resultadoJogador < resultadoComputador) || (resultadoJogador > 21)) && (resultadoComputador <= 21)) {
+                return ("O computador ganhou!")
+            } else
+                return ("Vocês empataram.")
+    }
+}
